6505d64133b1188cd618fb9f40bbf8ed3f4d1ac0
luan.lazzari <luanlazzari@hotmail.com>
2020-07-19 21:29:20 -0300

    refactor: move form status to a separate file

---
+++ src/presentation/components/formStatus/form-status.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Styles from './form-status-styles.scss'
+import Spinner from '@/presentation/components/spinner/spinner'
+
+type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+
+const FormStatus: React.FC<Props> = (props: Props) => {
+  return (
+    <div className={Styles.errorWrap}>
+      <Spinner className={Styles.spinner} />
+      <span className={Styles.error}>Erro</span>
+    </div>
+  )
+}
+
+export default FormStatus
