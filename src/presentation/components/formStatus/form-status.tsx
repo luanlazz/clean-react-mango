e56b39f2e25f64a07c5de12be37360398a62b749
luan.lazzari <luanlazzari@hotmail.com>
2020-07-20 00:58:17 -0300

    feat: ensure Login will render spinner and error on start

--- src/presentation/components/formStatus/form-status.tsx
+++ src/presentation/components/formStatus/form-status.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Styles from './form-status-styles.scss'
 import { Spinner } from '@/presentation/components'
+import Context from '@/presentation/contexts/form/form-context'
 
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const FormStatus: React.FC<Props> = (props: Props) => {
+  const { isLoading, errorMessage } = useContext(Context)
+
   return (
-    <div className={Styles.errorWrap}>
-      <Spinner className={Styles.spinner} />
-      <span className={Styles.error}>Erro</span>
+    <div data-testid='error-wrap' className={Styles.errorWrap}>
+      {isLoading && <Spinner className={Styles.spinner} />}
+      {errorMessage && <span className={Styles.error}>{errorMessage}</span>}
     </div>
   )
 }
