5f3cdc1696b8ab16c826abe3f427bde377b1e3cd
luanlazz <luanlazzari@hotmail.com>
2020-10-08 22:29:52 -0300

    feat: ensure RemoteAddAccount throws EmailInUseError if HttpPostClient returns 403

--- src/data/usecases/add-account/remote-add-account.ts
+++ src/data/usecases/add-account/remote-add-account.ts
@@ -1,6 +1,7 @@
 import { AddAccount, AddAccountParams } from '@/domain/usecases'
 import { AccountModel } from '@/domain/models'
-import { HttpPostClient } from '@/data/protocols/http'
+import { EmailInUseError } from '@/domain/errors'
+import { HttpPostClient, HttpStatusCode } from '@/data/protocols/http'
 
 export class RemoteAddAccount implements AddAccount {
   constructor (
@@ -9,10 +10,14 @@ export class RemoteAddAccount implements AddAccount {
   ) {}
 
   async add (params: AddAccountParams): Promise<AccountModel> {
-    await this.httpClient.post({
+    const httpResponse = await this.httpClient.post({
       url: this.url,
       body: params
     })
-    return null
+
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.forbidden: throw new EmailInUseError()
+      default: return null
+    }
   }
 }
